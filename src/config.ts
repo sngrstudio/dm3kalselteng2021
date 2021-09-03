ef134e015616115f2f2e524f959a55e8158ba2ac
R. Ilham Sastronegoro <radenpioneer@users.noreply.github.com>
2021-09-04 00:28:11 +0700

    change node version to lts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,12 @@
+const devUrl = 'http://localhost:8888'
+const prodUrl = 'https://https://dm3kalselteng2021.netlify.app'
+
+const isProd = process.env.NODE_ENV === 'production' ? true : false
+
 export const site = {
     name: 'DM3 KAMMI Kalsel-Kalteng 2021',
+    url: isProd ? prodUrl : devUrl,
+    logo: '/images/logo.png'
 }
 
 export const menus = [
